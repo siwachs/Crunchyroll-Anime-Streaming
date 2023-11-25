f11f836b3a007045be76c11085c71fd50ad9ae46
siwachs <ssiwach.8888@gmail.com>
2023-11-26 02:30:09 +0530

    HeroCarousel refactored

--- src/assets/Slider/index.ts
+++ src/assets/Slider/index.ts
@@ -1,13 +1,26 @@
-import image1 from './1.jpg';
-import image1Name from './01.webp';
+// Import images
+import image1 from "./1.jpg";
+import image1Name from "./01.webp";
 
-import image2 from './2.webp';
-import image2Name from './02.webp';
+import image2 from "./2.webp";
+import image2Name from "./02.webp";
 
-import image3 from './3.webp';
-import image3Name from './03.webp';
+import image3 from "./3.webp";
+import image3Name from "./03.webp";
 
-import image4 from './4.jpg';
-import image4Name from './04.webp';
+import image4 from "./4.jpg";
+import image4Name from "./04.webp";
 
-export { image1, image1Name, image2, image2Name, image3, image3Name, image4, image4Name };
+interface ImageObject {
+  image: string;
+  imageName: string;
+}
+
+const images: ImageObject[] = [
+  { image: image1, imageName: image1Name },
+  { image: image2, imageName: image2Name },
+  { image: image3, imageName: image3Name },
+  { image: image4, imageName: image4Name },
+];
+
+export { images };
--- src/components/HeroCarousel/index.tsx
+++ src/components/HeroCarousel/index.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
+import styles from "./HeroCarousel.module.css";
+
+import { images } from "../../assets/Slider";
 
 const HeroCarousel: React.FC = () => {
-  return <div></div>;
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  return (
+    <div>
+      <div className={styles.hero_carousel_card_wrapper}>
+        <div className={styles.hero_carousel_item}>
+          <div className={styles.hero_carousel_card}>
+            <div className={styles.hero_carousel_layout}>
+              <div className={styles.hero_carousel_layout_grid}></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroCarousel;
