03cce63b3fac4bf7c0fe28b5d1136242a04ef715
siwachs <ssiwach.8888@gmail.com>
2024-11-22 13:41:02 +0530

    dataFeedCarousel

--- src/app/(main)/_components/dataFeedRow/dataFeedCarousel.tsx
+++ src/app/(main)/_components/dataFeedRow/dataFeedCarousel.tsx
@@ -1,8 +1,52 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+
+import images from "@/assets/dataFeed";
+
 const DataFeedCarousel = () => {
   return (
-    <div className="container-cmp wide-cards-carousel-container px-0"></div>
+    <div className="container-cmp wide-cards-carousel-layout px-0">
+      <div className="container-cmp wide-cards-carousel-container">
+        {/* Nav Left */}
+
+        <div className="wide-cards-carousel-frame">
+          <div className="carousel-scroller">
+            <div className="carousel-scroller-wrapper">
+              <div className="carousel-scroller-track">
+                {images.map((image) => (
+                  <div key={image.key} className="carousel-scroller-card">
+                    <div className="browse-card">
+                      <Link href="/" tabIndex={-1} className="relative block">
+                        <div className="browse-card-image browse-card-image-sized">
+                          <div className="browse-card-image-wrapper">
+                            <div className="browse-card-image-sizer">
+                              <Image
+                                sizes="(min-width: 2160px) calc(100vw / 7), (min-width: 1720px) calc(100vw / 6), (min-width: 800px) calc(100vw / 5), (min-width: 568px) calc(100vw / 4), (min-width: 480px) calc(100vw / 3), 50vw"
+                                src={image.image}
+                                alt={image.title}
+                                className="block size-full object-cover"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </Link>
+                    </div>
+
+                    <div className="browse-card-body"></div>
+
+                    <div className="browse-card-hover"></div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Nav Right */}
+      </div>
+    </div>
   );
 };
 
--- src/assets/dataFeed/index.ts
+++ src/assets/dataFeed/index.ts
@@ -1,19 +1,23 @@
 import image1 from "./0.avif";
 import image2 from "./1.avif";
-import image3 from "./2.jpeg";
+import image3 from "./2.avif";
 import image4 from "./3.avif";
 import image5 from "./4.avif";
 import image6 from "./5.avif";
 import image7 from "./6.avif";
 
 const images = [
-  { image: image1, title: "Fairy Tail" },
-  { image: image2, title: "Solo Leveling" },
-  { image: image3, title: "JoJo's Bizarre Adventure" },
-  { image: image4, title: "Frieren: Beyond Journey's End" },
-  { image: image5, title: "Chainsaw Man" },
-  { image: image6, title: "KONOSUBA -God's blessing on this wonderful world!" },
-  { image: image7, title: "Fruits Basket" },
+  { key: 1, image: image1, title: "Fairy Tail" },
+  { key: 2, image: image2, title: "Solo Leveling" },
+  { key: 3, image: image3, title: "SPY x FAMILY" },
+  { key: 4, image: image4, title: "Frieren: Beyond Journey's End" },
+  { key: 5, image: image5, title: "Chainsaw Man" },
+  {
+    key: 6,
+    image: image6,
+    title: "KONOSUBA -God's blessing on this wonderful world!",
+  },
+  { key: 7, image: image7, title: "Fruits Basket" },
 ];
 
 export default images;
