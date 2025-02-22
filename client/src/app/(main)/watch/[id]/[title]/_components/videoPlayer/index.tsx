4875eb24f7e2a6814c949a0d50d5be0d04f4b289
siwachs <ssiwach.8888@gmail.com>
2025-02-22 12:36:41 +0530

    Building Player

--- client/src/app/(main)/watch/[id]/[title]/_components/videoPlayer/index.tsx
+++ client/src/app/(main)/watch/[id]/[title]/_components/videoPlayer/index.tsx
@@ -1,29 +1,54 @@
-import Image from "next/image";
+"use client";
 
-import wideImage from "@/assets/watch/image-wide.jpg";
+import { useRef, useState } from "react";
+
+import { FaPlay, FaPause } from "react-icons/fa";
+
 import "./index.css";
 
-const VideoPlayer: React.FC<{ media: string }> = ({ media }) => {
+const VideoPlayer = ({ media }: { media: string }) => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  const [isMediaPlaying, setIsMediaPlaying] = useState(false);
+
+  function toogleIsMediaPlaying() {
+    setIsMediaPlaying((prev) => !prev);
+  }
+
   return (
-    <div className="video-player-wrapper">
-      <div className="video-player-spacer" />
+    <div className="video-player relative grid w-full">
+      <div className="video-player-sizer pointer-events-none h-[56.25vw] max-h-40 min-h-[calc(20rem*0.5625)]" />
 
-      <div className="content-wrapper relative w-full">
-        <div className="content-wrapper image-wrapper">
-          <figure className="relative size-full">
-            <Image
-              fill
-              priority
-              sizes="100vw"
-              src={wideImage}
-              alt="Solo Leveling"
-              className="block size-full object-cover"
-            />
-          </figure>
-        </div>
+      <div className="relative size-full">
+        <video ref={videoRef} className="video absolute size-full" />
 
-        <div className="content text-center text-xl select-none">
-          Video Goes Here
+        <div className="controls absolute size-full">
+          <button
+            className="pause-or-play absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer text-white"
+            onClick={toogleIsMediaPlaying}
+          >
+            {isMediaPlaying ? (
+              <FaPause className="size-12" />
+            ) : (
+              <FaPlay className="size-12" />
+            )}
+          </button>
+
+          <div className="duration-and-seek absolute right-0 bottom-0 left-0 px-5 py-2.5">
+            <div className="duration mb-3 flex items-center justify-between text-xs">
+              <span>00:00</span>
+              <span>00:00</span>
+            </div>
+
+            <div className="seek relative h-0.5 bg-[var(--meta-color)]">
+              <input
+                min="0"
+                type="range"
+                className="seek-progress h-full bg-[var(--app-background-crunchyroll-orange)]"
+                style={{ width: "50%" }}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
